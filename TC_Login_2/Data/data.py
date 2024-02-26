b1f21a17febc85588afef0f53e90031f6f70cf21
tisisuma <152303037+tisisuma@users.noreply.github.com>
2024-02-26 10:27:32 +0530

    Delete TC_Login_2 directory

--- TC_Login_2/Data/data.py
+++
@@ -1,6 +0,0 @@
-class Web_Data:
-   url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-   username = "Admin"
-   password = "admin12"
-   error="Invalid credentials"
-   dashboardurl ="https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
