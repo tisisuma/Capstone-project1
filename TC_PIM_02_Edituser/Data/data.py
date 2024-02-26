0ce53673ff7221e605c56f67972b3c1b038129be
tisisuma <152303037+tisisuma@users.noreply.github.com>
2024-02-26 10:27:58 +0530

    Delete TC_PIM_02_Edituser directory

--- TC_PIM_02_Edituser/Data/data.py
+++
@@ -1,7 +0,0 @@
-class Web_Data:
-   url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-   username = "Admin"
-   password = "admin123"
-   dashboardurl ="https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
-
-   name="uma"
--- TC_PIM_02_Edituser/Locators/locators.py
+++
@@ -1,9 +0,0 @@
-class Web_Locators:
-   username_locator = "username"
-   password_locator = "password"
-   submit_button_locator = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
-   pim_locator = '//a[@href="/web/index.php/pim/viewPimModule"]'
-   edit_locator ='//i[@class="oxd-icon bi-pencil-fill"]'
-   firstname_locator="firstName"
-   scroll_locator='//div[@clear="false"]'
-   savebtn_locator='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[5]/button'
