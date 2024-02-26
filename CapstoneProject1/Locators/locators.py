9b4d1337fc3a93f1a5b897d289b6383a8cb20847
tisisuma <152303037+tisisuma@users.noreply.github.com>
2024-02-26 09:53:50 +0530

    Update code

---
+++ CapstoneProject1/Locators/locators.py
@@ -0,0 +1,35 @@
+class Web_Locators:
+   username_locator = "username"
+   password_locator = "password"
+   submit_button_locator = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
+   error_locator = "//p[text()='Invalid credentials']"
+   pim_locator = '//a[@href="/web/index.php/pim/viewPimModule"]'
+   add_locator = '//button[@class="oxd-button oxd-button--medium oxd-button--secondary"]'
+   empname_locator = "firstName"
+   lastname_locator = "lastName"
+   Nickname_locator = '//input[@class="oxd-input oxd-input--active"]'
+   Saveemp_locator = '//button[@type="submit"]'
+   nickname_locator = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div[2]/div/div/div[2]/input'
+   otherid_locator = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[1]/div[2]/div/div[2]/input'
+   license_locator = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[2]/div[1]/div/div[2]/input'
+   licenseexp_locator = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[2]/div[2]/div/div[2]/div/div/i'
+   year_locator = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[2]/div[2]/div/div[2]/div/comment()'
+   month_locator = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[2]/div[2]/div/div[2]/div/comment()'
+   ssn_locator = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[3]/div[1]/div/div[2]/input'
+   sinno_locator = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[3]/div[2]/div/div[2]/input'
+   nationality_locatot = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[1]/div[1]/div/div[2]/div/div/div[2]/i'
+   maritalstatus_locator = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[1]/div[2]/div/div[2]/div/div/div[2]/i'
+   doblocator = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[2]/div[1]/div/div[2]/div/div/i'
+   dobyear_locator = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[2]/div[1]/div/div[2]/div/comment()'
+   dobmonth_locator = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[2]/div[1]/div/div[2]/div/comment()'
+   gender_locator = '//label[text()="Female"]'
+   militaryservice_locator = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[4]/div/div[1]/div/div[2]/input'
+   smoker_locator = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[4]/div/div[2]/div/div[2]/div/label'
+   save_info_locator = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[5]/button'
+   edit_locator = '//i[@class="oxd-icon bi-pencil-fill"]'
+   firstname_locator = "firstName"
+   scroll_locator = '//div[@clear="false"]'
+   savebtn_locator = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[5]/button'
+   delete_locator = '//i[@class="oxd-icon bi-trash"]'
+   deletebtn_locator = '//button[text()=" Yes, Delete "]'
+   deletemsg_locator = '//div[@class="oxd-toast-container oxd-toast-container--bottom"]'
