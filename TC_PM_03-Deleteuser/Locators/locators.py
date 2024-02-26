ec0036c7dd70c95aa656600066ebeb5f763ca4b0
tisisuma <152303037+tisisuma@users.noreply.github.com>
2024-02-26 10:28:09 +0530

    Delete TC_PM_03-Deleteuser directory

--- TC_PM_03-Deleteuser/Locators/locators.py
+++
@@ -1,8 +0,0 @@
-class Web_Locators:
-   username_locator = "username"
-   password_locator = "password"
-   submit_button_locator = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
-   pim_locator = '//a[@href="/web/index.php/pim/viewPimModule"]'
-   delete_locator ='//i[@class="oxd-icon bi-trash"]'
-   deletebtn_locator='//button[text()=" Yes, Delete "]'
-   deletemsg_locator='//div[@class="oxd-toast-container oxd-toast-container--bottom"]'
