f64373eca529f13ddec872a3e74e5b1f8aa3a2f6
tisisuma <152303037+tisisuma@users.noreply.github.com>
2024-02-26 10:27:09 +0530

    Delete TC_Login_1 directory

--- TC_Login_1/Locators/locators.py
+++
@@ -1,4 +0,0 @@
-class Web_Locators:
-   username_locator = "username"
-   password_locator = "password"
-   submit_button_locator = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
